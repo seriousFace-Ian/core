07714d7dcae4aa0f4f09025fd0489ffdce3c5872
seriousFace-Ian <ianchoi.cn@outlook.com>
2026-04-15 18:29:21 +0800

    build: config input output path

---
+++ packages/core/src/index.ts
@@ -0,0 +1,15 @@
+export {default as EventsEmitter} from './event/events'
+export {default as debounce} from './function/debounce'
+export {default as throttle} from './function/throttle'
+export {
+  isArray,
+  isBoolean,
+  isFunction,
+  isNull,
+  isNumber,
+  isObject,
+  isString,
+  isSymbol,
+  isUndefined,
+  parseType,
+} from './is/is'
